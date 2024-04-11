c2641e9b4aeba5544f3b791b38321e21e9af30b9
Dobid <davidolivares68@gmail.com>
2024-04-11 11:08:49 +0200

    added JSBSim env creation

--- tdmpc2/envs/jsbsim.py
+++ tdmpc2/envs/jsbsim.py
@@ -1,12 +1,13 @@
 import gymnasium as gym
 import jsbgym
 
+
 def make_env(cfg):
     """
         Make an env for TD-MPC2 experiments in the JSBSim simulator.
     """
-    env = gym.make('ACNoVaIntegErr-v0', config_file = '../../../jsbsim_cfg.yaml', telemetry_file = 'telemetry.csv',
-                   render_mode = 'none')
+    env = gym.make('ACNoVaIntegErr-v0', config_file = 'jsbsim_cfg.yaml', telemetry_file = 'telemetry/telemetry.csv',
+                   render_mode = cfg.render_mode)
     # env = gym.make('SimpleAC_OMAC-v0', config_file = '../../../jsbsim_cfg.yaml', telemetry_file = 'telemetry.csv',
     #                render_mode = 'none')
     env = gym.wrappers.RecordEpisodeStatistics(env)
