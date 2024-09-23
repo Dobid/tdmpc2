af7db80b15b6de934256de476c5cd2fe4bda98df
Dobid <davidolivares68@gmail.com>
2024-09-23 16:39:32 +0200

    quick fix for compatibility of JsbGym env creation according to legacy env creation

--- tdmpc2/envs/jsbsim.py
+++ tdmpc2/envs/jsbsim.py
@@ -6,11 +6,14 @@ def make_env(cfg):
     """
         Make an env for TD-MPC2 experiments in the JSBSim simulator.
     """
-    env = gym.make(f'{cfg.rl.task}-v0', cfg_env=cfg.env, telemetry_file='telemetry/telemetry.csv',
+    try:
+        env = gym.make(f'{cfg.rl.task}-v0', cfg_env=cfg.env, telemetry_file='telemetry/telemetry.csv',
                    render_mode=cfg.env.jsbsim.render_mode)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+    except:
+        raise ValueError(f'Unknown task: {cfg.rl.task}')
     # env = gym.make('SimpleAC_OMAC-v0', config_file = '../../../jsbsim_cfg.yaml', telemetry_file = 'telemetry.csv',
     #                render_mode = 'none')
-    env = gym.wrappers.RecordEpisodeStatistics(env)
 
     print("observation space: ", env.observation_space.shape)
     print("action space: ", env.action_space.shape)
