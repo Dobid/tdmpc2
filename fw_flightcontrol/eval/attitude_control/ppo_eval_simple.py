c6d0c5acced752553aec235f5481eb481730b337
Dobid <davidolivares68@gmail.com>
2024-12-13 16:27:43 +0100

    created task subdirs in eval dir

--- fw_flightcontrol/eval/attitude_control/ppo_eval_simple.py
+++ fw_flightcontrol/eval/attitude_control/ppo_eval_simple.py
@@ -9,7 +9,7 @@ from omegaconf import DictConfig
 from fw_flightcontrol.agents import ppo
 from fw_flightcontrol.utils.train_utils import make_env
 
-@hydra.main(version_base=None, config_path="../config", config_name="default")
+@hydra.main(version_base=None, config_path="../../config", config_name="default")
 def eval(cfg: DictConfig):
     np.set_printoptions(precision=3)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -38,10 +38,10 @@ def eval(cfg: DictConfig):
     ppo_agent.eval()
 
     # load the reference sequence and initialize the evaluation arrays
-    simple_ref_data = np.load(f'eval/refs/{cfg.ref_file}.npy')
+    simple_ref_data = np.load(f'eval/attitude_control/refs/{cfg.ref_file}.npy')
 
     # load the jsbsim seeds to apply at each reset and set the first seed
-    jsbsim_seeds = np.load(f'eval/refs/jsbsim_seeds.npy')
+    jsbsim_seeds = np.load(f'eval/attitude_control/refs/jsbsim_seeds.npy')
     cfg_sim.eval_sim_options.seed = float(jsbsim_seeds[0])
 
     # if no render mode, run the simulation for the whole reference sequence given by the .npy file
@@ -81,12 +81,11 @@ def eval(cfg: DictConfig):
         ep_step = 0 # step counter within an episode
         step = 0
         refs = simple_ref_data[ep_cnt]
-        roll_ref, pitch_ref = refs[0], refs[1]
         # set default target values
-        # roll_ref = np.deg2rad(55)
-        # pitch_ref = np.deg2rad(28)
+        # refs[0] = np.deg2rad(30)
+        # refs[1] = np.deg2rad(15)
         while step < total_steps:
-            env.set_target_state(roll_ref, pitch_ref)
+            env.set_target_state(refs)
             action = ppo_agent.get_action_and_value(obs)[1].squeeze_(0).detach().cpu().numpy()
             obs, reward, terminated, truncated, info = env.step(action)
             if cfg_task.mdp.obs_is_matrix:
@@ -114,7 +113,6 @@ def eval(cfg: DictConfig):
                 eps_fcs_fluct.append(np.mean(np.abs(np.diff(ep_fcs_pos_hist, axis=0)), axis=0)) # get fcs fluctuation of the episode and append it to the list of all fcs fluctuations
                 if ep_cnt < len(simple_ref_data):
                     refs = simple_ref_data[ep_cnt]
-                roll_ref, pitch_ref = refs[0], refs[1]
             ep_step += 1
             step += 1
 
